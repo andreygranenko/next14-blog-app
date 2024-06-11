2e38ff622d473b92740957112f79601af6abdfc8
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-06-11 18:21:55 +0300

    updated about and contact page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,31 @@
+import styles from './about.module.css';
+import Image from "next/image";
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page</p>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h3 className={styles.subTitle}>About Agency</h3>
+        <h2 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better</h2>
+        <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid architecto deleniti dolorem doloremque eveniet fugit hic ipsam labore mollitia ratione reiciendis, veniam voluptas. Accusamus autem natus numquam reprehenderit temporibus.</p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h2>10 K+</h2>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h2>100+</h2>
+            <p>Projects completed</p>
+          </div>
+          <div className={styles.box}>
+            <h2>50+</h2>
+            <p>Happy clients</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <Image src={"/about.png"} alt={'about'} fill className={styles.aboutImg}/>
+      </div>
     </div>
   );
 }
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,21 @@
+import styles from './contact.module.css'
+import Image from "next/image";
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>Send us a message!</p>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={'/contact.png'} alt={'contact us'} fill className={styles.contactImg}/>
+      </div>
+      <form className={styles.formContainer}>
+        <h3 className={styles.subTitle}>Contact Us</h3>
+        <h2 className={styles.title}>We are here to help you</h2>
+        <input type="text" placeholder="Name and Surname" className={styles.input}/>
+        <input type="email" placeholder="Email adress" className={styles.input}/>
+        <input type="number" placeholder="Phone number (Optional)" className={styles.input}/>
+        <textarea placeholder="Your Message" className={styles.input}></textarea>
+        <button className={styles.btn}>Send Message</button>
+
+      </form>
     </div>
   );
 }
